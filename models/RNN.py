99614f906afa92f62406cb0ec9d472996c9bc6c4
Megan Frisella <meganfrisella@gmail.com>
2021-11-18 17:53:06 -0500

    Filled out transformer model. Added embedding step to RNN and transformer.

--- models/RNN.py
+++ models/RNN.py
@@ -2,15 +2,19 @@ import numpy as np
 import tensorflow as tf
 
 class RNN(tf.keras.Model): 
-    def __init__(self, window_size, alphabel_size):
+    def __init__(self, window_size=10, alphabel_size=37, embedding_size=10):
         super(RNN, self).__init__()
         self.window_size = window_size
         self.alphabel_size = alphabel_size
+        self.embedding_size = embedding_size
         self.learning_rate = 1e-3
         self.batch_size = 128
         
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
         
+        self.emb_ciphertext = tf.keras.layers.Embedding(self.alphabet_size, self.embedding_size)
+        self.emb_plaintext = tf.keras.layers.Embedding(self.alphabet_size, self.embedding_size)
+
         self.encoder = tf.keras.layers.LSTM(128, return_sequences=True, return_state=True)
         self.decoder = tf.keras.Sequential([
             tf.keras.layers.LSTM(128),
@@ -18,11 +22,17 @@ class RNN(tf.keras.Model):
         ])
     
     def call(self, ciphertext, plaintext):
+        # embed the ciphertext characters
+        ciphertext_embs = self.emb_ciphertext(ciphertext)
+
         # encode the ciphertext in a cell_state/hidden_state
-        _, cell_state, hidden_state = self.encoder(ciphertext, initial_state=None)
+        _, cell_state, hidden_state = self.encoder(ciphertext_embs, initial_state=None)
+
+        # embed the plaintext characters
+        plaintext_embs = self.emb_plaintext(plaintext)
         
         # decode the plaintext given an encoding of the ciphertext by teacher forcing
-        probs = self.decoder(plaintext, initial_state=(cell_state, hidden_state))
+        probs = self.decoder(plaintext_embs, initial_state=(cell_state, hidden_state))
         
         return probs
     
